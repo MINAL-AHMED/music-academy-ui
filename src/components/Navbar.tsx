0b81ed463df0abb801eb3fda11056ad06318deca
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-20 16:22:23 +0600

    complete hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 font-bold",
+        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 font-semibold",
         className
       )}
     >
